767b1e55baa6005c434c55e8958029d6e58edf54
Mariola Hullings <mariolahullings@gmail.com>
2025-01-21 11:20:02 -0500

    InputToDo

--- client/src/components/InputToDo.js
+++ client/src/components/InputToDo.js
@@ -2,16 +2,36 @@ import React, { Fragment, useState } from "react";
 
 const InputTodo = () => {
   const [description, setDescription] = useState("");
+  
+  //sent an event
+ const onSubmitForm = async(e) => {
+    //do not want to refresh
+    e.preventDefault();
+    try {
+    const body = { description };
+                    //send request fetch- makes a get request
+    const response = await fetch("http://localhost:5001/todos/", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body)
+    });
+
+    console.log(response);
+    } catch (err) {
+     console.error(err.message)   ;
+    }
+ }
 
   return (
     <Fragment>
       <h1 className="text-center mt-5">Pern Todo List</h1>
-      <form className="d-flex mt-5">
+      <form className="d-flex mt-5" onSubmit={onSubmitForm}>
         <input
           type="text"
           className="form-control"
           value={description}
-          onChange={(e) => setDescription(e.target)}
+          //change update description e-event target the input and get the value of it
+          onChange={e => setDescription(e.target.value)}
         />
         <button className="btn btn-success"> Add</button>
       </form>
