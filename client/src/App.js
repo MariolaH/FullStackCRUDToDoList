a96ea899c7dd3db38edc0fb19dd9ee38920c3da8
Mariola Hullings <mariolahullings@gmail.com>
2025-01-21 15:50:56 -0500

    Added EDIT, DELETE, UPDATE

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 
 // components
 import InputTodo from './components/InputToDo';
+import ListTodos from './components/ListTodos';
 
 function App() {
   return (
  <Fragment>
   <div className='container'>
 <InputTodo />
+<ListTodos />
 
   </div>
 
